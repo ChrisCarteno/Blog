0e0a1decea1d02161708f439bb7c3aabe105aef0
Chrispy <christiancarteno@hotmail.com>
2022-10-18 06:24:15 -0700

    carousel added, categories pages added

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,11 @@ import { FeaturedPosts } from '../sections'
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      < FeaturedPosts />
       <Head>
-        <title>Zia Mia's Blog</title>
+        <title>Zia Mia's Blooog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1> something somthing, wth this isn't working</h1>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post) => <PostCard post={post.node}  key={post.title}/>)}
